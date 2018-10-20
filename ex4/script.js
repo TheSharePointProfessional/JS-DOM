0bd7b1f2dfeda28eb8a0a5beb1ac69d69c690617
RenteriaL <renterial@thesharepointprofessional.com>
2018-10-20 18:53:02 -0500

    complete ex4/5

--- ex4/script.js
+++ ex4/script.js
@@ -1,2 +1,11 @@
 // Make all of the boxes blue when you click the button
 // Notice that all the boxes have "class" instead of "id"
+var button = document.getElementById("myButton");
+
+button.addEventListener('click', function() {
+    var x = document.getElementsByClassName("box");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.backgroundColor = "blue";
+    }
+})
