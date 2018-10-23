02d86fe4befabcf587cc64b453c2e0623de23297
RenteriaL <renterial@thesharepointprofessional.com>
2018-10-23 18:31:45 -0500

    complete ex7

--- ex7/script.js
+++ ex7/script.js
@@ -1,3 +1,12 @@
 // Make the section disappear and reappear whenever you click the section-header
 // BONUS: Can you make the section slide up and down like this? :
 // https://www.w3schools.com/bootstrap/bootstrap_collapse.asp
+var accordion = document.getElementById("accordion");
+accordion.querySelector("h3").addEventListener('click', function() {
+    var section = document.getElementById('section');
+    if (section.style.display === "none") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+});
